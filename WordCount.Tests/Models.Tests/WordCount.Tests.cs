24f39c60d8a42b7848fa54a1e41cefc5df4eca39
Rakhee Gandhi <rpgandhi@gmail.com>
2017-11-02 16:02:33 -0700

    Add functionality to method and pass test.

--- WordCount.Tests/Models.Tests/WordCount.Tests.cs
+++ WordCount.Tests/Models.Tests/WordCount.Tests.cs
@@ -9,11 +9,49 @@ namespace WordCount.Tests
   [TestClass]
   public class RepeatCounterTest
   {
+
+    // [TestMethod]
+    // public void CountRepeats_OutputsHowManyInstancesofInputWordAreInInputSentence_Int()
+    // {
+    //   //Arrange
+    //   string sentence = "It is dark and cold outside.";
+    //   string word = "and";
+    //   RepeatCounter testInput = new RepeatCounter(sentence, word);
+    //
+    //   //Act
+    //   string result = testInput
+    // }
+
     [TestMethod]
-    public void CountRepeats_UserIsAbleToInputWords_True()
+    public void CountRepeats_CountInstancesofInputtedWordInInputtedSentence_One()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(true, testRepeatCounter.CountRepeats(2));
+      //Arrange
+      string sentence = "The blue sky and the birds are blue.";
+      string word = "blue";
+      RepeatCounter testSentence = new RepeatCounter(sentence, word);
+
+      //Act
+      int result = testSentence.CountRepeats(sentence, word);
+
+      //Assert
+      Assert.AreEqual(2, result);
     }
+
+    // [TestMethod]
+    // public void CountRepeats_UserIsAbleToInputSentence_True()
+    // {
+    //   RepeatCounter testSentence = new RepeatCounter();
+    //   string sentenceInput = testSentence.GetSentenceInput("hello");
+    //   // RepeatCounter testRepeatCounter = new RepeatCounter();
+    //   Console.WriteLine(sentenceInput);
+    //   Assert.IsInstanceOfType(sentenceInput, typeof(string));
+    // }
+
+    // [TestMethod]
+    // public void CountRepeats_UserIsAbleToInputSentence_True()
+    // {
+    //   RepeatCounter testRepeatCounter = new RepeatCounter();
+    //   Assert.AreEqual(true, testRepeatCounter.CountRepeats(2));
+    // }
   }
 }
--- WordCount/Models/WordCount.cs
+++ WordCount/Models/WordCount.cs
@@ -7,27 +7,89 @@ namespace WordCount.Models
 
   public class RepeatCounter
   {
-    private string _input;
-    // private string _word;
+    private string _sentence;
+    private string _word;
+    private int _counter;
 
-    public RepeatCounter ()
+    public RepeatCounter (string sentence, string word, int counter = 0)
     {
-      _input = input;
-      // _word = word;
+      _sentence = sentence;
+      _word = word;
+      _counter = counter;
     }
 
-    public static string GetSentenceInput()
+    public int CountRepeats(string sentence, string word)
     {
-      return _name;
+      string [] phrase = sentence.Split(new Char [] {' ', ',', '.', ':', '/', '\t', ';', '?', '!', '#', });
+      // string _sentence = sentence;
+      // string _word = word;
+      // string [] phrase = sentence.Split(new Char [] {' ', ',', '.', ':', '/', '\t', ';', '?', '!', '#', });
+      int counter = 0;
+
+      foreach (string currentWord in phrase)
+      {
+        if (currentWord == word)
+        {
+          counter++;
+        }
+      }
+
+      // for (int i = 0; i <= sentence.Length; i++)
+      // {
+      //   if(phrase.Contains(word)== true)
+      //   {
+      //     counter ++;
+      //   }
+      // }
+      return counter;
     }
 
-    public void SetSentenceInput(string)
-
-    public static string CountRepeats(string input, string word)
-    {
-
-      return false;
-    }
+    // public string RepeatCounter(string word, string sentence)
+    // {
+    //   string _word = word;
+    //   string _sentence = sentence;
+    //   int result = 0;
+    //
+    //   sentence = sentence.Trim();
+    //   if (sentence == '')
+    //   {
+    //     return 0;
+    //   }
+    //   while (sentence.Contains(" "))
+    //   {
+    //     sentence = sentence.Replace(" ", " ");
+    //   }
+    //
+    // }
+    //
+    // public string GetSentenceInput(string hello)
+    // {
+    //   string sentence = "hello";
+    //   return sentence;
+    //   // return _sentence;
+    // }
+    //
+    // public void SetSentenceInput(string newSentence)
+    // {
+    //   _sentence = newSentence;
+    //
+    // }
+    //
+    // public static string GetWordInput()
+    // {
+    //   return _word;
+    // }
+    //
+    // public void SetWordInput(string newWord)
+    // {
+    //   _word = newWord;
+    // }
+    //
+    // public static string CountRepeats(string input, string word)
+    // {
+    //
+    //   return false;
+    // }
 
   }
 }
